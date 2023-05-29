971d7d90fd333f2afc75f84138e82ed4e4e30f1d
Van Hien <hiench2001@gmail.com>
2023-05-29 14:16:05 -0600

    feat: add new students to student.txt

--- app.py
+++ app.py
@@ -1,16 +1,36 @@
 from student import Student
-from choice import Choice
 
-def student_modifier():
-    student = Student(input("Please enter the student name: "), input("Please enter the student major: "), input("Please enter the student GPA: "))
-    student_list = open("student.txt", "a")
-    student_list.write("Name: " + str(student.name) + "\n")
-    student_list.write("Major: " + str(student.major) + "\n")
-    student_list.write("GPA: " + str(student.gpa) + "\n")
-    student_list.write("On Probation: " + str(student.on_probation()) + "\n\n")
-    student_list.close()
+option = (
+    "1. Add Student\n"
+    "2. Change Student\n"
+    "3. Delete Student\n"
+    "4. Exit"
+)
+
+def user_options():
+    choice = 0
+    print(option)
+    try:
+        choice = int(input("Enter your choice of action: ")) 
+    except ValueError as err:
+        print(err)    
+    while choice != 4:    
+        if choice == 1:
+            def student_modifier():
+                student = Student(input("Please enter the student name: "), input("Please enter the student major: "), input("Please enter the student GPA: "))
+                student_list = open("student.txt", "a")
+                student_list.write("Name: " + str(student.name) + "\n")
+                student_list.write("Major: " + str(student.major) + "\n")
+                student_list.write("GPA: " + str(student.gpa) + "\n")
+                student_list.write("On Probation: " + str(student.on_probation()) + "\n\n")
+                student_list.close()  
+            student_modifier() 
+                                
+user_options()
+
+
     
-student_modifier()             
+          
 
     
    
--- choice.py
+++
@@ -1,4 +0,0 @@
-class Choice:
-    def __init__(self, choice, desc):
-        self.choice = choice
-        self.desc = desc
