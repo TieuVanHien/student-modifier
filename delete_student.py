551c4353987c6743a3bc8739548d3d4b1f07d6bf
Van Hien <hiench2001@gmail.com>
2023-05-31 10:41:52 -0600

    fix: updated delete function to not delete the key

--- delete_student.py
+++ delete_student.py
@@ -2,30 +2,23 @@ import json
 
 def delete_student():
     student_name = input("Please enter a student name that you want to delete: ")
-    update_lines = []
     found_student = False
-    
-    with open("student.json", "r") as file:
+
+    with open("student.json", "r+") as file:
         data = json.load(file)
-        for student in data["student"]:
-            name = student["name"]
-            if name.startswith(student_name):
+        students = data["student"]
+        for student in students:
+            if student["name"] == student_name:
+                students.remove(student)
                 found_student = True
-            else:
-                update_lines.append(student)
-    
+                break
+
+        file.seek(0)
+        json.dump(data, file, indent=4)
+        file.truncate()
+
     if found_student:
-        with open("student.json", "w") as file:
-            json.dump(update_lines, file)
         print("Successfully deleted.")
     else:
-        print("Failed to delete or student not found.")
+        print("Student not found.")
 
-# def print_json():
-#     with open("student.json", "r") as file:
-#         data = json.load(file)
-#         for student in data["student"]:
-#             name = student["name"]
-#             print(name)
-    
-# print_json()    
